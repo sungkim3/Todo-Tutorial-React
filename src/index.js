0086991cde87115c94ea2b6796d6cd05f98aabe4
Sung Kim <sungkim@MacBook-Pro.local>
2019-06-27 22:34:56 -0700

    implemented highlighting

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square(props) {
     return (
-        <button className="square" onClick={props.onClick}>
+        <button style={props.styles} className="square" onClick={props.onClick}>
             {props.value}
         </button>
     );
@@ -13,9 +13,11 @@ function Square(props) {
 class Board extends React.Component {
     renderSquare(i) {
         return (
-            <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />
+            <Square styles={this.props.squares[i].backgroundColor} value={this.props.squares[i].value} onClick={() => this.props.onClick(i)} />
         );
     }
+
+    
   
     render() {
         return (
@@ -45,11 +47,14 @@ class Game extends React.Component {
         super(props);
         this.state = {
             history: [{
-                squares: Array(9).fill(null),
-                index: 0,
+                squares: Array(9).fill({
+                    backgroundColor: { backgroundColor: 'white' },
+                    value: null
+                }),
+                index: null,
             }],
             stepNumber: 0,
-            xIsNext: true,
+            xIsNext: true
         }
     }
 
@@ -57,31 +62,83 @@ class Game extends React.Component {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
+        if (calculateWinner(squares) || squares[i].value) {
             return;
         }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        const xOption = {
+            backgroundColor: { backgroundColor: 'white' },
+            value: 'X'
+        }
+        const oOption = {
+            backgroundColor: { backgroundColor: 'white' },
+            value: 'O'
+        }
+        squares[i] = this.state.xIsNext ?  xOption : oOption;
         this.setState({
             history: history.concat([{
                 squares: squares,
-                index: i
+                index: i,
             }]),
             stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
+            
         });
     }
 
-    jumpTo(step) {
+    jumpTo(step, index) {
         // method to reset state when jumping to a move
         // step is the move number, 0 means no moves have been made yet
+        console.log('jumping...');
+        console.log(step);
+        console.log(index);
+        const history = this.state.history.slice(0, step + 1);
+        console.log(history);
+        // const current = history[history.length - 1];
+        // console.log(current);
+        // const squares = current.squares.slice();
+        // history[history.length - 1].squares = squares;
+
         this.setState({
             stepNumber: step,
             xIsNext: (step % 2) === 0,
-        })
+            history: history
+        });
+    }
+
+    highlightSquare(index) {
+        let history = this.state.history.slice();
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        const newColor = { backgroundColor: 'red' };
+        squares[index].backgroundColor = newColor;
+
+        history[history.length - 1].squares = squares;
+        
+        this.setState({
+            history: history,
+        });
+
+    }
+
+    unHighlightSquare(index) {
+        let history = this.state.history.slice();
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        const newColor = { backgroundColor: 'white' };
+        squares[index].backgroundColor = newColor;
+
+        history[history.length - 1].squares = squares;
+        
+        this.setState({
+            history: history,
+        });
+
     }
 
     render() {
+        console.log('render game');
         const history = this.state.history;
+        console.log(history);
         const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
         const moves = history.map((step, move) => {
@@ -91,7 +148,11 @@ class Game extends React.Component {
             const desc = move ? 'Go to move #' + move + ' Coord: ('+col+','+row+')' : 'Go to game start';
             return (
                 <li key={move}>
-                    <button onClick={() => this.jumpTo(move, index)}>{desc}</button>
+                    <button 
+                    onClick={() => this.jumpTo(move, step.index)}
+                    onMouseEnter={() => this.highlightSquare(step.index)}
+                    onMouseLeave={() => this.unHighlightSquare(step.index)}
+                    >{desc}</button>
                 </li>
             );
         });
@@ -126,20 +187,20 @@ document.getElementById('root')
 
 function calculateWinner(squares) {
     const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
     ];
     for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
+        const [a, b, c] = lines[i];
+        if (squares[a].value && squares[a].value === squares[b].value && squares[a].value === squares[c].value) {
+            return squares[a].value;
+        }
     }
     return null;
-  }
+}
